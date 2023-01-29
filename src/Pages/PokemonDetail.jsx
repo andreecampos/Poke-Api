321be15c4398c253889b874c0b4a4644f7766d8a
andreecampos <orlandoandree1998@gmail.com>
2023-01-29 18:25:36 +0100

    CSS for detail-page and some CSS for responsive

--- src/Pages/PokemonDetail.jsx
+++ src/Pages/PokemonDetail.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import axios from "axios";
 
 const PokemonDetail = () => {
   const [selectedPokemon, setSelectedPokemon] = useState([]);
@@ -8,24 +7,15 @@ const PokemonDetail = () => {
   // const [isLoading, setIsLoading] = useState(false);
 
   const { name } = useParams();
+  const type = selectedPokemon.types && selectedPokemon.types[0].type.name;
+  const style = `card-body-detail ${type}`;
 
   const loadDetail = async () => {
-    // setIsLoading(true);
-    // axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
-    //   .then((res) => {
-    //     setSelectedPokemon(res.data);
-    //     setIsLoading(false);
-    //   })
-    //   .catch((err) => console.log(err));
-    // setIsLoading(true);
     try {
       const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
       const data = await res.json();
-      // console.log("data", data);
-      // const singelPokemon = [];
-      // singelPokemon.push(data);
+
       setSelectedPokemon(data);
-      // setIsLoading(false);
     } catch (err) {
       console.log(err);
     }
@@ -35,34 +25,26 @@ const PokemonDetail = () => {
     loadDetail();
   }, []);
 
-  // if (isLoading) {
-  //   return <p>Loading...</p>;
-  // }
-  // if (!selectedPokemon) {
-  //   return <p>Error</p>;
-  // }
   return (
-    <div>
-      <h1>PokemonDetail</h1>
-      <h2>{selectedPokemon.name}</h2>
-      {selectedPokemon.sprites && (
-        <img src={selectedPokemon.sprites.front_default} alt="" />
-      )}
-      {/* {isLoading ? (
-        <h1>loading</h1>
-      ) : (
-        selectedPokemon.map((item) => {
-          return (
-            <>
-              <h1>{item.name}</h1>
-              <img src={item.sprites.front_default} alt="" />
-            </>
-          );
-        })
-      )} */}
-
-      <p>Weight: {selectedPokemon.weight}</p>
-      <p>Height: {selectedPokemon.height}</p>
+    <div className="pokedex-grid-detail">
+      <div className="thumb-container-detail">
+        {selectedPokemon.sprites && (
+          <img
+            src={selectedPokemon.sprites.other.dream_world.front_default}
+            alt=""
+            className="pokemon-img-detail"
+          />
+        )}
+      </div>
+      <div className={style}>
+        <h5>#{selectedPokemon.id}</h5>
+        <h2 className="card-title-detail">{selectedPokemon.name}</h2>
+        <p>Weight: {selectedPokemon.weight}</p>
+        <p>Height: {selectedPokemon.height}</p>
+        <p>
+          Type: {selectedPokemon.types && selectedPokemon.types[0].type.name}
+        </p>
+      </div>
     </div>
   );
 };
